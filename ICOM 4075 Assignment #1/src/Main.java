bad1852488eaaf917276c29a703f1f5e5b7b3845
RonaldVelez <ronald.velez@upr.edu>
2017-03-26 09:10:44 +0800

    Update Main.java
    Lab #5 Main

--- ICOM 4075 Assignment #1/src/Main.java
+++ ICOM 4075 Assignment #1/src/Main.java
@@ -1,4 +1,19 @@
 
-public class Main {
+import javax.swing.JFrame;
 
+public class Main {
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Mine Sweeper");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(800, 350);
+		myFrame.setSize(340, 375);
+
+		MyPanel myPanel = new MyPanel();
+		myFrame.add(myPanel);
+
+		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
+		myFrame.addMouseListener(myMouseAdapter);
+
+		myFrame.setVisible(true);
+	}
 }
